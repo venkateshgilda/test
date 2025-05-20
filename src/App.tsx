a26012d63e42e174cec84a110ba0ed424c65ff58
Ubuntu <ubuntu@ip-172-31-22-228.ec2.internal>
2025-05-20 11:56:06 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to ${{ values.name }}!</h1>
+      <p>This is a React app scaffolded with Backstage.</p>
+    </div>
+  );
+};
+
+export default App;
